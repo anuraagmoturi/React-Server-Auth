5571a9a98ba13e1f74fdd0233890de50bb8b01ee
anuraagmoturi <anuraagmoturivb@gmail.com>
2018-04-27 15:32:06 -0400

    Passport-Jwt

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,4 +1,11 @@
+const jwt = require('jwt-simple');
 const User = require('../models/user');
+const config = require('../config');
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
 
 exports.signup = function (req, res, next) {
   const email = req.body.email;
@@ -28,17 +35,19 @@ exports.signup = function (req, res, next) {
       if(err){ return next(err); }
 
       //Respond to request indicating the user was created
-      res.json(user);
+      res.json({token: tokenForUser(user)});
 
     });
 
   });
 
+};
 
+exports.signin = function (req, res, next) {
+  //User has already had their email and password auth'd
+  //we just need to give them a token
 
-
-
-
+  res.send({token: tokenForUser(req.user)});
 
 
 };
--- router.js
+++ router.js
@@ -1,5 +1,17 @@
 const Authentication = require('./controllers/authentication');
+const passportService = require('./services/passport');
+const passport = require('passport');
+
+//we don't want session created by passport as we are using jwt
+const requireAuth = passport.authenticate('jwt', { session: false });
+const requireSignin = passport.authenticate('local', {session: false})
 
 module.exports = function (app) {
-  app.post('/signup', Authentication.signup)
+  app.get('/', requireAuth, function (req, res) {
+    res.send({ hi: 'there'});
+  });
+
+  app.post('/signup', Authentication.signup);
+
+  app.post('/signin', requireSignin, Authentication.signin);
 };
